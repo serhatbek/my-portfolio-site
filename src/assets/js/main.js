79f241c86d125f59f91fe83b532a128f595db409
serhatbek <serhat.bekk@gmail.com>
2021-03-28 02:35:49 +0300

    banner animations

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,3 +8,19 @@ function toggleNavBar() {
   headerNavigation.classList.toggle('openNav');
   toggleBurgerBtn.classList.toggle('close');
 }
+
+// Animations
+
+const bannerLeftContent = document.querySelector('.banner__content');
+const bannerRightContent = document.querySelector('.banner__img');
+
+window.onload = function () {
+  bannerLeftContent.style.opacity = 0;
+  bannerRightContent.style.opacity = 0;
+  setTimeout(() => {
+    bannerLeftContent.classList.add('fadeInLeft');
+    bannerRightContent.classList.add('fadeInRight');
+    bannerLeftContent.style.opacity = 1;
+    bannerRightContent.style.opacity = 1;
+  }, 1000);
+};
